b88f8981e8fd80677523f8161b95349cda5168df
NightCruiser <cruiserspb@gmail.com>
2020-07-14 04:51:07 +0300

    harjoitus

---
+++ LUT7/t3/io.h
@@ -0,0 +1,8 @@
+#ifndef IO_H
+#define IO_H
+
+void arvoNumerot(int *, int);
+int tarkistaNumero(int *, int, int);
+void tulostaNumerot(int *, int);
+
+#endif /*IO_H*/
---
+++ LUT7/t3/muisti.h
@@ -0,0 +1,7 @@
+#ifndef MUISTI_H
+#define MUISTI_H
+
+int *varaaMuistiaNumeroille(int);
+void vapautaMuisti(int *);
+
+#endif /*MUISTI_H*/
